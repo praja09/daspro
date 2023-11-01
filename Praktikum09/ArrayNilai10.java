87d2cd46a27b6385a4a68c56d1c5c66af5074bf0
praja09 <dipaprajadpp@gmail.com>
2023-11-01 15:00:59 +0700

    array nilai modif

--- Praktikum09/ArrayNilai10.java
+++ Praktikum09/ArrayNilai10.java
@@ -7,15 +7,17 @@ public class ArrayNilai10 {
         int[] nilaiAkhir = new int[10];
 
         for (int i = 0; i < nilaiAkhir.length; i++) {
-            System.out.print("Masukkan nilai akhir mahasiswa ke-" + (i + 1) + " : ");
+            System.out.print("Masukkan nilai akhir mahasiswa ke-" + (i+1) + " : ");
             nilaiAkhir[i] = input.nextInt();
         }
         for (int i = 0; i < 10; i++) {
             if (nilaiAkhir[i] > 70) {
-                System.out.println("Nilai akhir mahasiswa ke-" + (i + 1) + " lulus ");
-            } else {
-                System.out.println("Nilai akhir mahasiswa ke-" + (i + 1) + "tidak lulus ");
+                System.out.println("Mahasiswa ke-" + (i+1) + " lulus ");
+            }
+            if (nilaiAkhir[i] < 70) {
+                System.out.println("Mahasiswa ke-" + (i+1) + " tidak lulus ");
             }
         }
     }
 }
+
