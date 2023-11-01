d7f7078ba3f05a71a700d8b966cc63228df337c4
praja09 <dipaprajadpp@gmail.com>
2023-11-01 16:46:30 +0700

    array nilai modif terakhir

--- Praktikum09/ArrayNilai10.java
+++ Praktikum09/ArrayNilai10.java
@@ -2,22 +2,50 @@ import java.util.Scanner;
 
 public class ArrayNilai10 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        int[] nilaiAkhir = new int[10];
+    Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < nilaiAkhir.length; i++) {
-            System.out.print("Masukkan nilai akhir mahasiswa ke-" + (i+1) + " : ");
-            nilaiAkhir[i] = input.nextInt();
-        }
-        for (int i = 0; i < 10; i++) {
-            if (nilaiAkhir[i] > 70) {
-                System.out.println("Mahasiswa ke-" + (i+1) + " lulus ");
-            }
-            if (nilaiAkhir[i] < 70) {
-                System.out.println("Mahasiswa ke-" + (i+1) + " tidak lulus ");
-            }
-        }
+    int[] nilaiAkhir =  new int[10];
+    int totalNilai = 0;
+    int nilaiTertinggi = 0;
+    int nilaiTerendah = 100;
+    
+    for (int i = 0; i < nilaiAkhir.length; i++) {
+        System.out.println("Masukkan nilai akhir mahasiswa ke-" + (i+1) + ":");
+        nilaiAkhir[i] = sc.nextInt();
+        totalNilai += nilaiAkhir[i];
+     
+     
+    // nilai tertinggi nilai terendah 
+
+     if (nilaiAkhir[i] > nilaiTertinggi){
+        nilaiTertinggi = nilaiAkhir[i];
+     }
+     if (nilaiAkhir[i] < nilaiTerendah){
+        nilaiTerendah = nilaiAkhir[i];
+     }
+
     }
-}
 
+    double nilaiRatarata = totalNilai / nilaiAkhir.length;
+     int jumlahLulus = 0;
+     int jumlahTidakLulus = 0;
+
+    for (int i = 0; i < 10; i++) {
+        if (nilaiAkhir[i] > 70){
+             System.out.println("Mahasiswa ke-" + (i+1) + " lulus ");
+             jumlahLulus++;
+        }
+        if (nilaiAkhir[i] < 70) {
+            System.out.println("Mahasiswa ke-" + (i+1) + " tidak lulus ");
+            jumlahTidakLulus++;
+        }
+        }
+
+        System.out.println("Nilai Rata-Rata : " + nilaiRatarata);
+        System.out.println("Nilai Terendah : " + nilaiTerendah);
+        System.out.println("Nilai Tertinggi : " + nilaiTertinggi);
+        System.out.println("Jumlah Mahasiswa Yang Lulus : " + jumlahLulus);
+        System.out.println("Jumlah Mahasiswa Tidak Lulus : " + jumlahTidakLulus);
+}
+}
