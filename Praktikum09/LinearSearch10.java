46e83144b8a15c382c0cca41f0bef4af1c6a0fdd
praja09 <dipaprajadpp@gmail.com>
2023-11-01 15:28:46 +0700

    linear modif

--- Praktikum09/LinearSearch10.java
+++ Praktikum09/LinearSearch10.java
@@ -2,10 +2,22 @@ import java.util.Scanner;
 
 public class LinearSearch10 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
         int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        int key;
+        int hasil = -1;
+
+        System.out.print(" Masukkan jumlah elemen array : ");
+        int jumlahElemen = input.nextInt();
+        
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-" +i+ " : ");
+            arrayInt[i] = input.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari : ");
+        key = input.nextInt();
 
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
@@ -13,8 +25,8 @@ public class LinearSearch10 {
                 break;
             }
         }
-
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
-        
+        if (hasil == -1) {
+            System.out.println("key tidak ditemukan");
+        }
     }
 }
