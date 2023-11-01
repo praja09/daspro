8cff28e25ae7dc7dd524465b63f71f491b58a294
praja09 <dipaprajadpp@gmail.com>
2023-11-01 12:04:00 +0700

    array nilai

--- Praktikum09/ArrayNilai10.java
+++ Praktikum09/ArrayNilai10.java
@@ -1,7 +1,15 @@
 import java.util.Scanner;
 
-public class ArrayNilai10 
+public class ArrayNilai10 {
     public static void main(String[] args) {
-        
+        int[] nilaiAkhir = new int[10];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan nilai akhir mahasiswa ke-" + (i+1) + ": ");
+            nilaiAkhir[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir mahasiswa ke-" + (i+1) + " adalah " + nilaiAkhir[i] );
+        }
     }
 }
