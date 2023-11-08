211cbfc52e9b0fa891050424c0cbdd28968dada5
praja09 <dipaprajadpp@gmail.com>
2023-11-09 05:30:39 +0700

    latihan individu 2

--- Praktikum09/LatihanIndividu2.java
+++ Praktikum09/LatihanIndividu2.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
 public class LatihanIndividu2 {
+        public static void main(String[] args) {
+            Scanner input08 = new Scanner (System.in);
+            int[] arr = new int[10];
     
-}
+            for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+            }
+            System.out.print("Isi array secara terbalik: ");
+            for (int i = arr.length - 1; i >= 0; i--) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
