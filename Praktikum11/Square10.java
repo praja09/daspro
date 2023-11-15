bd6f8f9131e84ff601cb1677d55e0c0f070fcdcd
praja09 <dipaprajadpp@gmail.com>
2023-11-15 11:00:11 +0700

    Square pertama

--- Praktikum11/Square10.java
+++ Praktikum11/Square10.java
@@ -1,13 +1,16 @@
 package Praktikum11;
 import java.util.Scanner;
 
-public class Star10 {
+public class Square10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan jumlah *: ");
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i--) {
-            System.out.print("*");
-        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }     
     }
 }
