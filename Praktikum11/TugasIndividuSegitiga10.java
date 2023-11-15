b01f124fab7842e6b64baf229041cd744a416619
praja09 <dipaprajadpp@gmail.com>
2023-11-15 13:47:58 +0700

    Tugas Individu Segitiga

--- Praktikum11/TugasIndividuSegitiga10.java
+++ Praktikum11/TugasIndividuSegitiga10.java
@@ -1,7 +1,7 @@
 package Praktikum11;
 import java.util.Scanner;
 
-public class TugasIndividu10 {
+public class TugasIndividuSegitiga10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
