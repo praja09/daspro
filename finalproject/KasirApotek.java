5404eb628eeda5564d010625e843b5b3b4b2134a
praja09 <dipaprajadpp@gmail.com>
2023-11-24 13:09:12 +0700

    nyoba

--- finalproject/KasirApotek.java
+++ finalproject/KasirApotek.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class KasirApotek {
     public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
@@ -38,8 +37,5 @@ public class KasirApotek {
        System.out.print("Total Harga = " + totalHarga);
         
         sc.close();
-    }
-
-
-    
+    }   
 }
