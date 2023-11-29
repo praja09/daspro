7655e1794c2c6192c769158c133af6121f877f7f
praja09 <dipaprajadpp@gmail.com>
2023-11-29 10:50:01 +0700

    praktikum 13 (percobaan 1)

---
+++ Praktikum13/Welcome10.java
@@ -0,0 +1,10 @@
+package Praktikum13;
+
+public class Welcome10 {
+        public static void displayWelcomeMessage(){
+            System.out.println("Selamat datang di Politeknik Negeri Malang");
+}
+        public static void main(String[] args) {
+            displayWelcomeMessage();
+        }
+    }
