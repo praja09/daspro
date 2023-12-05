2b869482a0d69c6a71f13faebfe7743d16667afb
praja09 <dipaprajadpp@gmail.com>
2023-12-05 23:39:07 +0700

    tugas individu jobshet 13

--- Praktikum13/CobaArgs10.java
+++ Praktikum13/CobaArgs10.java
@@ -2,11 +2,14 @@ package Praktikum13;
 
 public class CobaArgs10 {
     public static void main(String[] args) {
+
+        if (1 < args.length) {
         System.out.println("Hai " + args[0]);
         System.out.println("Selamat datang di " + args[1]);
-    
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
+        } else {
+        // for (int i = 0; i < args.length; i++) {
+            // System.out.println(args[i]);
+            System.out.println("Hai , Selamat Datang ");
         }
     }
 }
---
+++ Praktikum13/SoalMateri.java
@@ -0,0 +1,49 @@
+package Praktikum13;
+
+import java.util.Scanner;
+public class SoalMateri {
+    
+    static String [] students = {"Sari", "Rina", "Yani", "Budi", "Ali"};
+    static int weeks = 3;
+    static int[][] nilai = new int[students.length][weeks];
+    
+    public static void main(String[] args) {
+        
+        inputNilai();
+        displayNilai();
+        nilaiTerbaik();
+    }
+    
+    public static void inputNilai(){
+        Scanner sc = new Scanner(System.in);
+        
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("Masukkan nilai " + students[i]);
+            for (int j = 0; j < weeks; j++) {
+                System.out.print("Minggu " + (j+1) + ": ");
+                nilai[i][j] = sc.nextInt();
+            }
+        }
+    }
+   public static void displayNilai(){
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("\nNilai " + (students[i]));
+            for (int j = 0; j < weeks; j++) {
+                System.out.println("Minggu " + (j + 1) + ": " + nilai[i][j]);
+            }
+        }
+
+    }
+
+    public static void nilaiTerbaik(){
+       for (int j = 0; j < weeks; j++) {
+            int max = nilai [0][j];
+            for (int i = 0; i < students.length; i++) {
+                if (nilai[i][j] > max) {
+                    max = nilai[i][j];
+                }
+            }
+            System.out.println("Nilai tertbaik minggu " + (j + 1) + ": " + max);
+       }
+    }
+}
